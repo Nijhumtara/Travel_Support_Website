08f5e1366d9f35121735f4e80f39dc1b71fb1373
Nijhumtara <nijhumtara1311@gmail.com>
2025-10-29 12:04:07 +0600

    feat: update travel insurance section structure and include new stylin g

--- travel_insurance.js
+++ travel_insurance.js
@@ -1,3 +1,5 @@
+// Functionality for travel_insurance page
+
 const navBar = document.querySelector("#nav_bar");
 const logo = document.querySelector("#logo");
 const travelDropDownIcon = document.querySelector("#travelDropDownIcon");
@@ -7,11 +9,13 @@ const travel_insurance_section = document.querySelector("#travel_insurance_secti
 
 let travelIsOpen = false;
 
+// Build functionality and include add necessary classes
+
 travelLink.addEventListener("click", () => {
   if (!travelIsOpen) {
     logo.src = "travel_logo.png";
     navBar.classList.add("travel_nav_bar");
-    travelDropDownIcon.classList.add("rotate");
+    travelDropDownIcon.classList.add("travel_rotate");
     navOptions.classList.add("travel_nav_bar_link");
     travel_insurance_section.style.display = "block";
     travelIsOpen = true;
@@ -19,9 +23,20 @@ travelLink.addEventListener("click", () => {
   else{
     logo.src = "logo.png";
     navBar.classList.remove("travel_nav_bar");
-    travelDropDownIcon.classList.remove("rotate");
+    travelDropDownIcon.classList.remove("travel_rotate");
     navOptions.classList.remove("travel_nav_bar_link");
     travel_insurance_section.style.display = "none";
     travelIsOpen = false;
   }
 });
+
+// const blackPart = document.querySelector('#black-part');
+// const infoContainer = document.querySelector("#info_container");
+
+// let infoheightIncrease = false;
+// travelLink.addEventListener('click', () => {
+//   if(infoContainer.style.height > "100"){
+//     blackPart.style.height = "700px";
+//   }
+// })
+
